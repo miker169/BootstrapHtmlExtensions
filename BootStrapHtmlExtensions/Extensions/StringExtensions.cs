a1eb799a907626b0fd0aa9ee921b262b65c32d1d
miker169 <miker169@hotmail.co.uk>
2013-04-16 21:40:19 +0100

    Initial project creation, includes initial copy of bootstrap extensions, in need of refactoring

---
+++ BootStrapHtmlExtensions/Extensions/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿
+namespace BootStrapHtmlExtensions.Extensions
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Sting extension methods
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Converts a string to an MVC Html String
+        /// </summary>
+        /// <param name="stringValue">The String Value</param>
+        /// <returns>An MVCHtlString</returns>
+        public static MvcHtmlString ToMvcHtmlString(this string stringValue)
+        {
+            return new MvcHtmlString(stringValue);
+        }
+
+
+    }
+}
---
+++ BootStrapHtmlExtensions/Extensions/TagBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace BootStrapHtmlExtensions.Extensions
+{
+    /// <summary>
+    /// Tag Builder Extension methods
+    /// </summary>
+    public static class TagBuilderExtensions
+    {
+        /// <summary>
+        /// Convert tagbuilder to an MVC HTML string.
+        /// </summary>
+        /// <param name="tagBuilder"> The tag builder.</param>
+        /// <returns>An MvcHtmlString</returns>
+        public static MvcHtmlString ToMvcHtmlString(this TagBuilder tagBuilder)
+        {
+            return tagBuilder.ToString().ToMvcHtmlString();
+        }
+
+        /// <summary>
+        /// Convert a tagbuilder to an MVC Html string.
+        /// </summary>
+        /// <param name="tagBuilder">The tage builder.</param>
+        /// <param name="tagRenderMode">The tag render mode</param>
+        /// <returns>An Mvc Html String</returns>
+        public static MvcHtmlString ToMvcHtmlString(this TagBuilder tagBuilder, TagRenderMode tagRenderMode)
+        {
+            return tagBuilder.ToString(tagRenderMode).ToMvcHtmlString();
+        }
+    }
+}
